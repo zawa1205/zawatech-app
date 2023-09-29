af7c7bd955be609f7e6a824f930b65ac3fff95c4
zawa <aizu.serizawa@gmail.com>
2023-09-29 13:09:56 +0900

    feat: Remove edge function

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,41 +1,42 @@
-import React from "react";
-import { ImageResponse, NextResponse } from "next/server";
+import React from 'react'
+import { ImageResponse } from 'next/server'
 
-export const runtime = "edge";
+// vercelの edge function の容量が1MBまでのためコメントアウト（2.27MB）
+// export const runtime = 'edge'
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const hasTitle = searchParams.has("title");
-  const title = hasTitle ? searchParams.get("title")?.slice(0, 50) : "";
-  const userName = "zawa1205";
+  const { searchParams } = new URL(request.url)
+  const hasTitle = searchParams.has('title')
+  const title = hasTitle ? searchParams.get('title')?.slice(0, 50) : ''
+  const userName = 'zawa1205'
   const fontData = await fetch(
-    new URL("./AkazukiPOP.woff", import.meta.url)
-  ).then((res) => res.arrayBuffer());
+    new URL('./AkazukiPOP.woff', import.meta.url),
+  ).then((res) => res.arrayBuffer())
 
   return new ImageResponse(
     (
       <div
         style={{
           fontSize: 60,
-          fontWeight: "bold",
-          color: "black",
-          display: "flex",
+          fontWeight: 'bold',
+          color: 'black',
+          display: 'flex',
           backgroundImage: `url(${`data:image/svg+xml,${encodeURIComponent(
-            '<svg id="visual" viewBox="0 0 1200 630" width="1200" height="630" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect width="1200" height="630" fill="#ffffff"></rect><g><g transform="translate(27 173)"><path d="M0 -142.9L123.7 -71.4L123.7 71.4L0 142.9L-123.7 71.4L-123.7 -71.4Z" fill="#bedcdd"></path></g><g transform="translate(843 311)"><path d="M0 -130L112.6 -65L112.6 65L0 130L-112.6 65L-112.6 -65Z" fill="#bedcdd"></path></g><g transform="translate(212 604)"><path d="M0 -48L41.6 -24L41.6 24L0 48L-41.6 24L-41.6 -24Z" fill="#bedcdd"></path></g><g transform="translate(1192 624)"><path d="M0 -102L88.3 -51L88.3 51L0 102L-88.3 51L-88.3 -51Z" fill="#bedcdd"></path></g><g transform="translate(1141 14)"><path d="M0 -108L93.5 -54L93.5 54L0 108L-93.5 54L-93.5 -54Z" fill="#bedcdd"></path></g><g transform="translate(387 2)"><path d="M0 -76L65.8 -38L65.8 38L0 76L-65.8 38L-65.8 -38Z" fill="#bedcdd"></path></g></g></svg>'
+            '<svg id="visual" viewBox="0 0 1200 630" width="1200" height="630" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect width="1200" height="630" fill="#ffffff"></rect><g><g transform="translate(27 173)"><path d="M0 -142.9L123.7 -71.4L123.7 71.4L0 142.9L-123.7 71.4L-123.7 -71.4Z" fill="#bedcdd"></path></g><g transform="translate(843 311)"><path d="M0 -130L112.6 -65L112.6 65L0 130L-112.6 65L-112.6 -65Z" fill="#bedcdd"></path></g><g transform="translate(212 604)"><path d="M0 -48L41.6 -24L41.6 24L0 48L-41.6 24L-41.6 -24Z" fill="#bedcdd"></path></g><g transform="translate(1192 624)"><path d="M0 -102L88.3 -51L88.3 51L0 102L-88.3 51L-88.3 -51Z" fill="#bedcdd"></path></g><g transform="translate(1141 14)"><path d="M0 -108L93.5 -54L93.5 54L0 108L-93.5 54L-93.5 -54Z" fill="#bedcdd"></path></g><g transform="translate(387 2)"><path d="M0 -76L65.8 -38L65.8 38L0 76L-65.8 38L-65.8 -38Z" fill="#bedcdd"></path></g></g></svg>',
           )}`})`,
-          width: "100%",
-          height: "100%",
-          padding: "50px 200px",
-          position: "relative",
-          textAlign: "center",
-          justifyContent: "center",
-          alignItems: "center",
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          position: 'relative',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <h2
           style={{
-            width: "100%",
-            fontWeight: "bold",
+            width: '100%',
+            fontWeight: 'bold',
             fontSize: 60,
             fontFamily: '"NotoSansJP"',
           }}
@@ -44,13 +45,13 @@ export async function GET(request: Request) {
         </h2>
         <div
           style={{
-            position: "absolute",
+            position: 'absolute',
             bottom: 0,
             right: 200,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            width: "100%",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            width: '100%',
           }}
         >
           <img
@@ -67,7 +68,7 @@ export async function GET(request: Request) {
     {
       width: 1200,
       height: 630,
-      fonts: [{ name: "AdobeBlank", data: fontData, style: "normal" }],
-    }
-  );
+      fonts: [{ name: 'AdobeBlank', data: fontData, style: 'normal' }],
+    },
+  )
 }
