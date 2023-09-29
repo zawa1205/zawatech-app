daba61ca73d694c0100880314f923671d6e60ae7
zawa <aizu.serizawa@gmail.com>
2023-09-29 11:29:18 +0900

    chore: Add sass option in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['localhost', 'github.com', 'zawatech.com'],
+  },
+  sassOptions: {
+    fiber: false,
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
 
